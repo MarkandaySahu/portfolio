56052ebf696a38b2d3c6a1b5d4d3139c63cf0924
markanday sahu <78273939+MarkandaySahu@users.noreply.github.com>
2025-03-17 17:34:33 +0530

    second commit

--- server.py
+++ server.py
@@ -1,6 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+import csv
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/submit",methods=['GET','POST'])
+def submit():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        m = "Form Submitted Successfully"
+        with open('db.csv', 'a', newline='') as csvfile:
+            db_writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            db_writer.writerow([name,email,message])
+        return render_template("submit.html",m=m)
+    else:
+        m = "Form not submitted"
+        return render_template("submit.html",m=m)
+
+#for any other page than home page
+@app.route('/<string:page_name>')
+def page(page_name='/'):
+    try:
+        return render_template(page_name)
+    except:
+        return redirect('/')
